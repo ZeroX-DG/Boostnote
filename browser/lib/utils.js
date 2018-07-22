121b1fad8836405b4b9ca06f8f5ed685bc327f3f
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-07-22 13:05:46 +0700

    updated comment in escape function

--- browser/lib/utils.js
+++ browser/lib/utils.js
@@ -20,8 +20,14 @@ export function escapeHtmlCharacters (html, opt = { detectCodeBlock: false }) {
     const current = { char: match[0], index: match.index }
     const codeBlockIndexs = []
     let openCodeBlock = null
+    // if the detectCodeBlock option is activated then this function should skip
+    // characters that needed to be escape but located in code block
     if (opt.detectCodeBlock) {
-      // position of the nearest line start
+      // The first type of code block is lines that start with 4 spaces
+      // Here we check for the \n character located before the character that
+      // needed to be escape. It means we check for the begining of the line that
+      // contain that character, then we check if there are 4 spaces next to the
+      // \n character (the line start with 4 spaces)
       let previousLineEnd = current.index - 1
       while (html[previousLineEnd] !== '\n' && previousLineEnd !== -1) {
         previousLineEnd--
@@ -33,18 +39,29 @@ export function escapeHtmlCharacters (html, opt = { detectCodeBlock: false }) {
         html[previousLineEnd + 3] === ' ' &&
         html[previousLineEnd + 4] === ' '
       ) {
-        // so skip it
+        // skip the current character
         continue
       }
-      // if the character is in ``` means, it's in a code block
-      // detecting code block
+      // The second type of code block is lines that wrapped in ```
+      // We will get the position of each ```
+      // then push it into an array
+      // then the array returned will be like this:
+      // [startCodeblock, endCodeBlock, startCodeBlock, endCodeBlock]
       while ((openCodeBlock = matchCodeBlockRegExp.exec(html)) !== null) {
         codeBlockIndexs.push(openCodeBlock.index)
       }
       let shouldSkipChar = false
+      // we loop through the array of positions
+      // we skip 2 element as the i index position is the position of ``` that
+      // open the codeblock and the i + 1 is the position of the ``` that close
+      // the code block
       for (let i = 0; i < codeBlockIndexs.length; i += 2) {
-        // this is an open ``` so index is the first ` and + 2 is the last `
-        // the second index is the closing ``` so the char must less than it
+        // the i index position is the position of the ``` that open code block
+        // so we have to + 2 as that position is the position of the first ` in the ````
+        // but we need to make sure that the position current character is larger
+        // that the last ` in the ``` that open the code block so we have to take
+        // the position of the first ` and + 2
+        // the i + 1 index position is the closing ``` so the char must less than it
         if (
           current.index > codeBlockIndexs[i] + 2 &&
           current.index < codeBlockIndexs[i + 1]
@@ -55,15 +72,21 @@ export function escapeHtmlCharacters (html, opt = { detectCodeBlock: false }) {
         }
       }
       if (shouldSkipChar) {
+        // skip the current character
         continue
       }
     }
     // otherwise, escape it !!!
     if (current.char === '&') {
+      // when escaping character & we have to be becareful as the & could be a part
+      // of an escaped character like &quot; will be came &amp;quot;
       let nextStr = ''
       let nextIndex = current.index
       let escapedStr = false
-      // maximum length of an escape string is 5. For example ('&quot;')
+      // maximum length of an escaped string is 5. For example ('&quot;')
+      // we take the next 5 character of the next string if it is one of the string:
+      // ['&quot;', '&amp;', '&#39;', '&lt;', '&gt;'] then we will not escape the & character
+      // as it is a part of the escaped string and should not be escaped
       while (nextStr.length <= 5) {
         nextStr += html[nextIndex]
         nextIndex++
